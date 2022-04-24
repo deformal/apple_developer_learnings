1db3de62e31a09e2913147fd90afd11a21a6c8eb
saurabh jainwal <drake9211@gmail.com>
2022-04-24 13:12:45 +0530

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by saurabh  on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
